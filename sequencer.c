13707b1aa708a9bafbca73ecfc2664ba71caade9
Johannes Schindelin <johannes.schindelin@gmx.de>
2018-08-24 15:07:41 +0200

    non-builtin rebase: use non-builtin interactive backend
    We recently converted both the `git rebase` and the `git rebase -i`
    command from Unix shell scripts to builtins.
    
    The former has a safety valve allowing to fall back to the scripted
    `rebase`, just in case that there is a bug in the builtin `rebase`:
    setting the config variable `rebase.useBuiltin` to `false` will
    fall back to using the scripted version.
    
    The latter did not have such a safety hatch.
    
    Let's reinstate the scripted interactive rebase backend so that `rebase.useBuiltin=false` will not use the builtin interactive rebase,
    just in case that an end user runs into a bug with the builtin version
    and needs to get out of the fix really quickly.
    
    This is necessary because Git for Windows wants to ship the builtin
    rebase/interactive rebase earlier than core Git: Git for Windows
    v2.19.0 will come with the option of a drastically faster (if a lot
    less battle-tested) `git rebase`/`git rebase -i`.
    
    As the file name `git-rebase--interactive` is already in use, let's
    rename the scripted backend to `git-legacy-rebase--interactive`.
    
    A couple of additional touch-ups are needed (such as teaching the
    builtin `rebase--interactive`, which assumed the role of the
    `rebase--helper`, to perform the two tricks to skip the unnecessary
    picks and to generate a new todo list) to make things work again.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- sequencer.c
+++ sequencer.c
@@ -4731,7 +4731,7 @@ out:
 }
 
 /* skip picking commits whose parents are unchanged */
-static int skip_unnecessary_picks(struct repository *r,
+int skip_unnecessary_picks(struct repository *r,
 				  struct todo_list *todo_list,
 				  struct object_id *output_oid)
 {
